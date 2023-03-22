a7999b41f3222aa913e0557602f27abd59cec262
Dipakkamble <kambledipak700@gmail.com>
2023-03-22 10:55:18 +0530

    [Dipak] create class and object

---
+++ OopsConcept/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    internal class Program
+    {
+        int id; string name;
+        static void Main(string[] args)
+        {
+            Program p1 = new Program();
+            p1.id = 101;p1.name = "sonoo jaiswal";
+            Console.WriteLine(p1.id);
+            Console.WriteLine(p1.name);
+            Console.ReadLine();
+        }
+    }
+}
